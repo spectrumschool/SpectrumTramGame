6191ebba82fd1de4eb7b5b512963af6f005110a5
Hannes Deville <hannes.deville@gmail.com>
2018-03-22 22:47:07 +0100

    added spoor, tram & singleton gamemanager

---
+++ Assets/Main/Scripts/TramDriver.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TramDriver : MonoBehaviour
+{
+	
+	private int currentTrack;
+
+	void Start()
+	{
+		//we start at track 1
+		ChangeTrack(1);
+	}
+
+	void Update ()
+	{
+		if(Input.GetKeyDown(KeyCode.LeftArrow))
+		{
+			//TODO: ChangeTrack(???);
+		}
+		if(Input.GetKeyDown(KeyCode.RightArrow))
+		{
+			//TODO: ChangeTrack(???);
+		}
+	}
+
+	void ChangeTrack(int newTrack)
+	{
+		//TODO: change position to track position...
+		//transform.position = new Vector3(?,?,?);
+	}
+}
