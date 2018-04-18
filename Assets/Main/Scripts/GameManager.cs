70218fd9d03e31f57b7cfa29358454b912104d5f
hannesdvl <hannes.deville@gmail.com>
2018-04-18 22:01:53 +0200

    passenger view, passenger stops, stop management

--- Assets/Main/Scripts/GameManager.cs
+++ Assets/Main/Scripts/GameManager.cs
@@ -4,16 +4,32 @@ using UnityEngine;
 
 public class GameManager : Singleton<GameManager>
 {
-	public int minTramSpeed = 1;
-	public int maxTramSpeed = 10;
-	public string[] haltenamen;
+	public float minTramSpeed = 1;
+	public float maxTramSpeed = 10;
+	public float distanceBetweenStops = 8;
+	public float passengerStopOffset = 4;
+	public float maxSpeedDistance = 100.0f;
+	public List<string> haltenamen;
 	public Transform[] tracks;
 	public Transform driverView;
 	public Transform passengersView;
 
-	[HideInInspector]
-	public int tramSpeed = 0;
+	public TramDriver tramDriver;
+	public SpawnManager spawnHalteDriver;
+	public SpawnManager spawnHaltePassenger;
 
+	public float tramSpeed = 0;
+
+	public float distanceTravelled {get; private set; }
+	float _nextStopDriver;
+	float _nextStopPassenger;
+	string[] _nextStopNames;
+
+	Transform _tfLastStopDriver;
+	Transform _tfLastStopPassenger;
+
+	Queue<string> _stopNamesQueue;
+	List<int> _shuffledTracks;
 	private int _reputation = 0;
 	public int reputation
 	{
@@ -25,6 +41,13 @@ public class GameManager : Singleton<GameManager>
 		}
 	}
 
+	void Awake()
+	{
+		_stopNamesQueue = new Queue<string>();
+		_nextStopNames = new string[4];
+		_shuffledTracks = new List<int>(){0,1,2,3};
+	}
+
 	void Start()
 	{
 		StartCoroutine( StartGameCR() );
@@ -32,6 +55,59 @@ public class GameManager : Singleton<GameManager>
 
 	void Update()
 	{
+		//update the distance travelled
+		distanceTravelled += Time.deltaTime * GameManager.inst.tramSpeed;
+		tramSpeed = Mathf.Lerp(minTramSpeed,maxTramSpeed,distanceTravelled/maxSpeedDistance);
+
+		if(distanceTravelled >= _nextStopDriver)
+		{
+			_nextStopDriver += distanceBetweenStops;
+
+
+//			float yVal = (_tfLastStopDriver == null ? distanceBetweenStops : _tfLastStopDriver.localPosition.y + distanceBetweenStops);
+
+			_shuffledTracks.Shuffle();
+			int numStops = UnityEngine.Random.Range(1,4);
+			for (int i = 0; i < 4; i++)
+			{
+				if(!string.IsNullOrEmpty(_nextStopNames[_shuffledTracks[i]]))
+				{
+					_stopNamesQueue.Enqueue(_nextStopNames[_shuffledTracks[i]]);
+				}
+
+				if(i < numStops)
+				{
+					//TODO: assign halte naam
+					_nextStopNames[_shuffledTracks[i]] = _stopNamesQueue.Dequeue();
+
+
+
+					_tfLastStopDriver = spawnHalteDriver.SpawnItem(new Vector2(tracks[_shuffledTracks[i]].localPosition.x, 5.0f)).transform;
+					_tfLastStopDriver.GetComponent<Halte>().haltenaam.text = _nextStopNames[_shuffledTracks[i]];
+				}
+				else
+				{
+					_nextStopNames[_shuffledTracks[i]] = "";
+				}
+			}
+		}
+
+		if(distanceTravelled >= _nextStopPassenger)
+		{
+			_nextStopPassenger += distanceBetweenStops;
+
+			//halte naam based on track
+
+			//if there is a name
+			if(!string.IsNullOrEmpty(_nextStopNames[tramDriver.currentTrack]))
+			{
+//				float xVal = (_tfLastStopPassenger == null ? distanceBetweenStops : _tfLastStopPassenger.localPosition.x + distanceBetweenStops);
+				//spawn halte Passenger
+				_tfLastStopPassenger = spawnHaltePassenger.SpawnItem(new Vector2(8.9f, 0)).transform;
+				_tfLastStopPassenger.GetComponent<Halte>().haltenaam.text = _nextStopNames[tramDriver.currentTrack];
+			}
+		}
+
 		//TEST
 		if(Input.GetButtonDown("P1"))
 		{
@@ -45,12 +121,22 @@ public class GameManager : Singleton<GameManager>
 
 	IEnumerator StartGameCR()
 	{
-		_reputation = 0;
-		EventManager.ReputationChangedEvent(_reputation);
-		tramSpeed = 0;
+		//keep a shuffled queue of names
+		haltenamen.Shuffle();
+		_stopNamesQueue.Clear();
+		_stopNamesQueue = new Queue<string>(haltenamen);
 
+		//reset values, stop tram
+		reputation = 0;
+		distanceTravelled = 0;
+		tramSpeed = 0;
+		_nextStopDriver = distanceBetweenStops;
+		_nextStopPassenger = distanceBetweenStops + passengerStopOffset;
+
+		//wait
 		yield return new WaitForSeconds(2.0f);
 
+		//start tram
 		tramSpeed = minTramSpeed;
 	}
 }
---
+++ Assets/Main/Scripts/TramPassengers.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TramPassengers : MonoBehaviour
+{
+	public float tredInterval = 1.0f;
+	public float tredDelta = 1.0f;
+	float _tredTimer;
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update ()
+	{
+		_tredTimer += Time.deltaTime * GameManager.inst.tramSpeed;
+		if(_tredTimer >= tredInterval)
+		{
+			_tredTimer -= tredInterval;
+			tredDelta *= -1.0f;
+			transform.Translate(0,tredDelta, 0);
+		}
+	}
+}
